3981497b73720bfaac76cf7908e9b533570f2f16
Pierre CURTO <pierre.curto@gmail.com>
2017-05-05 08:50:48 +0200

    improved doc

--- doc.go
+++ doc.go
@@ -1,29 +1,19 @@
 // Package construct provides a simple way to load configuration into a struct,
-// strongly relying on embedded types and interfaces defined on those.
+// from multiple sources, by relying on embedded types and interfaces defined on those.
 //
-// Data can be fetched from various sources in order of priority, overriding the
-// struct instance (default) values:
-//  - command line flags
-//  - environment variables
-//  - file in various formats
+// The goal is to reduce the code boilerplate used to initialize data structures to
+// a minimum, by leveraging Go's types syntax definitions, interface methods, types
+// reusability and by providing full support for complex field types (basic Go types, slices,
+// maps and any combination of those).
 //
-// The data sources are defined by implementing the relevant interfaces on the struct:
-//  - FromFlags interface for command line flags
-//  - FromEnv interface for environment variables
-//  - FromIO interface for io sources
+// Overview
 //
-// Subcommands in command line flags is supported and defined by embedding a struct
-// implementing both the Config and FromFlags interfaces. The FlagsDoneConfig() method
-// is invoked on the last subcommand with the remaining command line arguments.
-//
-// Once the data is loaded from all sources, the InitConfig() method is invoked
-// on the main struct as well as all the embedded ones except subcommands.
-//
-// The rules on the struct fields are as follow:
+// A struct has its fields populated by calling the Load() function.
+// The rules to populate the struct fields are as follow:
 //  - fields are only processed if they are exported
 //  - a field represents an option for the Config
+//  - an embedded type implementing the Config interface is used to group options logically
 //  - an embedded type implementing the Config and FromFlags interfaces represents a subcommand
-//  - an embedded type implementing the Config interface is used to group options
 //  - fields processing can be modified using field tags with the following format
 //
 //     `... cfg:"[<key>][,<flag1>[,<flag2>]]" ...`
@@ -38,4 +28,31 @@
 //                  not collide with the outer struct ones.
 //                  It has no effect on non embedded types.
 //
+// Subcommands
+//
+// Subcommands in command line flags is supported and defined by embedding a struct
+// implementing both the Config and FromFlags interfaces. The FlagsDoneConfig() method
+// is invoked on the last subcommand with the remaining command line arguments.
+//
+// Sources
+//
+// Data used to populate structs can be fetched from various sources in order of priority,
+// overriding the struct instance (default) values:
+//  - command line flags
+//  - environment variables
+//  - file in various formats
+//
+// The data sources are defined by implementing the relevant interfaces on the struct:
+//  - FromFlags interface for command line flags
+//  - FromEnv interface for environment variables
+//  - FromIO interface for io sources
+//
+// Once the data is loaded from all sources, the InitConfig() method is invoked
+// on the main struct as well as all the embedded ones except subcommands that have
+// not been requested.
+//
+// TODO
+//  - support comments in config files
+//  - expand variables? (cf yaml)
+//  - option to change any separator
 package construct
