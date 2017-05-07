6a62240b402beaa4499d9baa893055ff4e410683
Pierre CURTO <pierre.curto@gmail.com>
2017-05-07 08:38:36 +0200

    fixed unnamed types

--- internal/structs/structs.go
+++ internal/structs/structs.go
@@ -289,11 +289,6 @@ func fieldsOf(v interface{}, tagid, septagid string) (res []*StructField, err er
 			continue
 		}
 		field := vType.Field(i)
-		if field.Type.Name() == "" {
-			// unnamed type: no methods can be defined, ignore.
-			continue
-		}
-
 		fname := field.Name
 
 		tag := field.Tag
@@ -330,6 +325,11 @@ func fieldsOf(v interface{}, tagid, septagid string) (res []*StructField, err er
 			// Unsupported field types.
 			continue
 		case reflect.Struct:
+			if field.Type.Name() == "" {
+				// unnamed type: no methods can be defined, ignore.
+				continue
+			}
+
 			if field.Anonymous {
 				// Embedded field: recursively descend into its fields.
 				v := value.Addr().Interface()
